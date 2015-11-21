cac5419f5e0a4f7755b619aac9edb32a72662c53
billychorey <billychorey@gmail.com>
2015-11-21 14:56:33 -0500

    uploading first part of portfolio 11/21

---
+++ billychorey_site/zverse_site/js/script.js
@@ -0,0 +1,132 @@
+/* js file */
+
+(function( window, undefined ){
+
+
+	//This makes the available buttons change fill --
+	$( function(){
+
+		$(".availablebutton").click(function(){
+	        $(".onoff").toggleClass("orangefill");
+	    });
+	} );
+
+
+
+	//this is to toggle the billing button page
+	$( function(){
+
+		var billingbuttons = $( ".billingbuttons a" );
+		var payselect      = $( ".payselect" );
+		var pages          = $( ".page" );
+		
+		billingbuttons.on( "click", function( e ){
+
+			var atag      = $( this );
+			var classname = atag.data( "page" );
+
+			//changes the tab colors from gray to white when clicked
+	        payselect.addClass( "graybackground" );
+	        atag.parent().removeClass( "graybackground" );
+
+	        //hide the pages
+			pages.hide();
+			//show the page related to the button clicked
+			$( classname ).show();
+
+			//prevents the page from reloading
+	        e.preventDefault();
+	    });
+
+	    $( ".echeckbutton a" ).trigger( "click" );
+	} );
+
+
+	$( function(){
+
+		var projectbuttons = $( ".project_buttons a" );
+		var projectselect      = $( ".projectselect" );
+		var pages          = $( ".page" );
+		
+		projectbuttons.on( "click", function( e ){
+
+			var atag      = $( this );
+			var classname = atag.data( "page" );
+
+			//changes the tab colors from gray to white when clicked
+	        projectselect.addClass( "graybackground" );
+	        atag.parent().removeClass( "graybackground" );
+
+	        //hide the pages
+			pages.hide();
+			//show the page related to the button clicked
+			$( classname ).show();
+
+			//prevents the page from reloading
+	        e.preventDefault();
+	    });
+
+	    $( ".current_projects a" ).trigger( "click" );
+	} );
+
+
+	// this is to display or hide details on the projects page
+
+	$(document).ready(function(){
+    	
+    	$('.additionalinformation').hide();
+		
+		$("#show").click(function(){
+	        $(".additionalinformation").fadeIn("slow");
+	    });
+
+	    $("#hide").click(function(){
+	        $(".additionalinformation").hide();
+	    });
+    
+	});
+
+	$(document).ready(function(){
+    	
+    	$('.currentinformation').hide();
+		
+		$("#showcurrent").click(function(){
+	        $(".currentinformation").fadeIn("slow");
+	    });
+
+	    $("#hide_current").click(function(){
+	        $(".currentinformation").hide();
+	    });
+    
+	});
+
+	$(document).ready(function(){
+    	$("#declineOne").click(function(){
+	        $("#projectOne").hide();
+	    });
+		
+		$("#declineTwo").click(function(){
+	        $("#projectTwo").hide();
+	    });
+
+		$("#declineThree").click(function(){
+	        $("#projectThree").hide();
+	    });
+
+	    $("#declineDetailed").click(function(){
+	        $(".additionalinformation,#projectOne").hide();
+	    });
+	   
+    
+	});
+
+
+
+
+	// **********************
+
+	
+	
+
+
+})( window, undefined );
