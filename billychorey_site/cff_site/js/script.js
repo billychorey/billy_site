9ab3ccd5f5ac45e1b6cae30ddd96c295d7a8a3f6
billychorey <billychorey@gmail.com>
2015-11-30 13:21:09 -0500

    made changes in names of links, images, and class names

---
+++ billychorey_site/cff_site/js/script.js
@@ -0,0 +1,44 @@
+/* js file */
+
+( function( window, undefined ){ ///open closure  
+
+    $( function(){ //ready function
+
+
+        var list = $( ".navigation" ).find( "ul" );
+
+        list.hide();
+
+        $( ".mobile_nav_button" ).on( "click", function( e ){
+
+            list.slideToggle( 300 );
+            e.preventDefault();
+        } );
+
+        $( window ).on( "resize load", function( e ){
+
+        	if( $( window ).width() >= 700 ){
+
+        		list.removeAttr( "style" );
+        	}
+        } );
+
+    // beginning of trial jquery for donate
+
+	    var input = $( ".other").find("div");
+
+		input.hide();
+		$(".other_button").on("click",function(e){
+
+			input.slideToggle(300);
+			e.preventDefault();
+
+		});
+
+
+      
+	// end of test jquery
+
+    } ); //close of ready function
+
+} )( window, undefined ); ///close closure
